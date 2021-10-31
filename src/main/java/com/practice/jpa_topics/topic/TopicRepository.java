d22a817215df02bdcb6a32e31297a9cf38270c40
tehcGuy <scraptrashscrap@gmail.com>
2021-10-31 14:24:32 +0100

    Making the query by id number

--- src/main/java/com/practice/jpa_topics/topic/TopicRepository.java
+++ src/main/java/com/practice/jpa_topics/topic/TopicRepository.java
@@ -2,7 +2,7 @@ package com.practice.jpa_topics.topic;
 
 //interface that allows to use the CRUD functionality by extending CrudRepository
 
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 
 public interface TopicRepository extends CrudRepository<TopicToStudy, Integer> {
 }
